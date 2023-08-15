3bf5342d46a7fa0d3a2d6f726b67fbfa2d332086
rezaLin <rezanej666@gmail.com>
2023-08-15 17:38:34 +0330

    Add password and confirm password validation

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ let button=document.getElementById('dark-light');
 button.addEventListener('click',()=>
 {
     let className=document.documentElement.className;
-    document.documentElement.className=className==='light'?'dark':'light';
+    if(className==='light no-touch')
+        document.documentElement.className='dark';
+    else
+        document.documentElement.className=(className==='light')?
+        'dark':'light';
+}
+)
+let password=document.getElementById('password');
+let cpassword=document.getElementById('Cpassword');
+
+cpassword.addEventListener('input',()=>
+{
+
+    if(password.value!==cpassword.value)
+    {
+        cpassword.setCustomValidity('f');
+    }
+    else
+    cpassword.setCustomValidity('');
 }
 )
